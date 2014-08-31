8a67ec5f1a8ce09c3472b3d93cfb8a0ed33a04b1
MaximeLemay <maximelemay@scolab.com>
2014-08-31 12:01:53 -0400

    Project setup
    - Project with unit tests

---
+++ KingdomMapper/KingdomMapper/TileEnums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KingdomMapper
+{
+    class TileEnums
+    {
+        enum Terrain { 
+            Plains,
+            Forest,
+            Hills,
+            Mountains,
+            Swamp,
+            Desert,
+            Water
+        };
+
+        enum Road
+        {
+            None,
+            Road,
+            Highway
+        };
+    }
+}
